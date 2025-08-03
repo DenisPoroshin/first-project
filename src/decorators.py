b761b96951f50947fe16d4e2a50db3cc29be5a57
Денис Порошин <denis2102@mail.ru>
2025-08-03 21:43:33 +0700

    decorator log in decorators.py

---
+++ src/decorators.py
@@ -0,0 +1,35 @@
+from functools import wraps
+
+
+def log(filename=None):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                result = func(*args, **kwargs)
+                success_message = f"{func.__name__} completed successfully. Result: {result}"
+                if filename:
+                    with open(filename, "a", encoding="utf-8") as f:
+                        f.write(success_message + "\n")
+                else:
+                    print(success_message)
+                return result
+            except Exception as e:
+                error_message = f"Error in {func.__name__}: {type(e).__name__}. Input: {args}, {kwargs}"
+                if filename:
+                    with open(filename, "a", encoding="utf-8") as text:
+                        text.write(error_message + "\n")
+                else:
+                    print(error_message)
+
+        return wrapper
+
+    return decorator
+
+
+@log(filename="mylog.txt")
+def my_function(x, y):
+    return x + y
+
+my_function(4, 2)
+my_function(4, )
