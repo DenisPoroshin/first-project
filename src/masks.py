e6b3fd9b108f9b1fcfdee7a1b078b41022c14a1d
Денис Порошин <denis2102@mail.ru>
2025-08-21 13:24:33 +0700

    logger utils

--- src/masks.py
+++ src/masks.py
@@ -1,39 +1,41 @@
 import logging
 
 logging.basicConfig(
-    filename='../logs/masks.log',
-    filemode='w',
-    encoding='utf-8',
-    format='%(asctime)s %(filename)s %(levelname)s: %(message)s',
-    level=logging.DEBUG
+    filename="../logs/masks.log",
+    filemode="w",
+    encoding="utf-8",
+    format="%(asctime)s %(filename)s %(levelname)s: %(message)s",
+    level=logging.DEBUG,
 )
 
 logger = logging.getLogger(__name__)
 
+
 def get_mask_card_number(card_number: str) -> str:
     """Функция для маскировки номера карты"""
-    logger.info(f'Номер карты: {card_number}')
+    logger.info(f"Номер карты: {card_number}")
     if len(card_number) == 16 and card_number.isdigit():
-        logger.info('Применение маскировки')
+        logger.info("Применение маскировки...")
         masked_number = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
     else:
-        logger.error('Некорректный ввод')
+        logger.error("Некорректный ввод")
         raise ValueError("Некорректный ввод")
-    logger.info(f'Результат: {masked_number}')
+    logger.info(f"Результат: {masked_number}")
     return masked_number
 
 
 def get_mask_account(account_number: str) -> str:
     """Функция для маскировки номера счета"""
-    logger.info(f'Номер счета: {account_number}')
+    logger.info(f"Номер счета: {account_number}")
     if len(account_number) == 20 and account_number.isdigit():
-        logger.info('Применение маскировки.')
+        logger.info("Применение маскировки...")
         masked_account = f"**{account_number[-4:]}"
     else:
-        logger.error('Некорректный ввод')
+        logger.error("Некорректный ввод")
         raise ValueError("Некорректный ввод")
-    logger.info(f'Результат: {masked_account}')
+    logger.info(f"Результат: {masked_account}")
     return masked_account
 
-get_mask_card_number('1234567891234560')
-get_mask_account('12345678912345678900')
+
+get_mask_card_number("1234567891234560")
+get_mask_account("12345678912345678900")
--- src/utils.py
+++ src/utils.py
@@ -1,17 +1,36 @@
 import json
+import logging
+
+logging.basicConfig(
+    filename="../logs/utils.log",
+    filemode="w",
+    encoding="utf-8",
+    format="%(asctime)s %(filename)s %(levelname)s: %(message)s",
+    level=logging.DEBUG,
+)
+
+logger = logging.getLogger(__name__)
 
 
 def input_transaction(input_list):
     """Функция возвращает список словарей с данными о финансовых транзакциях
     из json-файла."""
+    logger.info('Запуск функции "reading_json_file"')
     try:
         with open(input_list, "r", encoding="utf-8") as f:
+            logger.info("Открытие файла...")
             transaction = json.load(f)
+            logger.info("Файл в формате списка")
         return transaction
     except json.JSONDecodeError:
+        logger.error("Файл не преобразовался")
         return []
     except FileNotFoundError:
+        logger.error("Файл не найден")
         return []
     except TypeError:
+        logger.error("Файл не в формате списка")
         return []
 
+
+input_transaction("")
